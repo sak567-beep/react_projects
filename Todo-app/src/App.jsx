9a32e684f377eb17cb268660b6dc44bb201c8b55
sakshishr <sakshisharma22092006@gmail.com>
2025-06-03 16:31:30 +0530

    first commit

---
+++ Todo-app/src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <center class="todo-container">
+      <h1>TODO  App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter Your Todo here" />
+            </div>
+            <div class="col-4"><input type="date" /></div>
+            <div class="col-2"><button type="button" class="btn btn-success">Add</button></div>
+          </div>
+          <div class="row">
+          <div class="col-6">
+      Buy Milk
+            </div>
+            <div class="col-4"> 4/05/2025</div>
+            <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
+          </div>
+          <div class="row">
+          <div class="col-6">
+            Go to school
+            </div>
+            <div class="col-4"> 05/05/2025</div>
+            <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
+          </div>
+          
+        </div>
+    </center>
+    );
+}
+export default App;
